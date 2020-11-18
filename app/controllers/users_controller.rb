f2e34f92c9bf8cf98e02550d0e1013be201169d9
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-11-18 02:30:54 -0800

    add actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_user, only: [:new, :create]
 
     def new
+        redirect_to user_reports_path(current_user) if current_user
+        @user = User.new
     end
 
     def create
+        @user = User.create(user_params)
+
+        if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to user_reports_path(@user)
+        else
+            flash[:signup_error] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
     end
 
 end
