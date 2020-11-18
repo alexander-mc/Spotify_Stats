615ffad2eab0ac2fb4d3b2d7ccf7a03ff00acc18
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-11-18 02:31:26 -0800

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ApplicationRecord
+
     has_many :reports
     has_many :songs, through: :reports
     has_many :artists, through: :songs
     has_many :albums, through: :songs
     has_many :genres, through: :songs
 
-    has_secure_password
+    validates :username,
+              presence: { message: "was not entered" },
+              format: { with: /\A[a-zA-Z0-9]*\z/, message: "can only contain letters and numbers" },
+              uniqueness: { case_sensitive: false, message: "is already taken" }
+    
+    has_secure_password (options = { validations: false })
+    # Set hsp validatons to false in order to customize (see custom validations below)
+
+    validates :password,
+              presence: { message: "was not entered" },
+              length: { minimum: 6, message: "must contain more than 6 characters"},
+              format: { without: /\s/, message: "cannot include spaces"},
+              confirmation: { message: "does not match password" }
+
+    scope :find_by_ci_username, -> (username) { where('lower("name") = ?', username.downcase).first }
+    #scope :ci_find, lambda { |attribute, value| where("lower(#{attribute}) = ?", value.downcase) }
+
 end
