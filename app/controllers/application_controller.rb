8da6a183a5d3a586d3e40ff461d2d13a2ec696c6
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-11-18 02:29:54 -0800

    add private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
-class ApplicationController < ActionController::Base
+ class ApplicationController < ActionController::Base
+    before_action :verify_user
+
+    private
+
+    def verify_user
+        redirect_to root_path unless current_user
+    end
+    
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
 end
