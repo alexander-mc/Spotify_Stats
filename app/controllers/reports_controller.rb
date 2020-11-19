e3b3050bdea61c21350126ced1674c841b474859
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-11-19 02:25:01 -0800

    draft actions for reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,27 @@
 class ReportsController < ApplicationController
     before_action :authenticate_user
 
+    def new
+        @report = Report.new
+    end
+
+    def create
+        binding.pry
+    end
+
     def index
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def udpate
+    end
+
+    def destroy
     end
 
 end
