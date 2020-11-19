2ab858bb2bb4864d9cf1eb04e6bb0d2bd6e0942a
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-11-19 00:50:06 -0800

    update user table

--- db/migrate/20201117004148_create_users.rb
+++ db/migrate/20201117004148_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :username
       t.string :password_digest
       t.string :spotify_username
+      t.string :spotify_id
       t.string :email
 
       t.timestamps
