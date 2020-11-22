a66005c376857c3d578e68c475751a672d4fdd42
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-11-22 07:24:33 -0800

    update Spotify user info - with comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,17 @@
     def current_user
         User.find_by(id: session[:user_id])
     end
-    
+
+    def logged_in?
+        !!current_user
+    end
+
     def redirect_if_logged_out
-        redirect_to root_path unless current_user   
+        redirect_to root_path unless logged_in?
     end
 
     def redirect_if_logged_in
-        redirect_to user_reports_path(current_user) if current_user
+        redirect_to user_reports_path(current_user) if logged_in?
     end
 
 end
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,47 +2,72 @@ class AuthenticationController < ApplicationController
 
     def callback
         binding.pry
+        
         # Store callback credentials
         session[:credentials] = request.env['omniauth.auth']
         auth_hash = session[:credentials]
-        #current_user = User.find_by(id: session[:user_id])
 
-        if current_user # is logged in
-            original_user = User.find_by(spotify_uid: auth_hash[:uid])
-            if current_user.spotify_uid.blank? && original_user
-                original_user.update(username: current_user.username, password_digest: current_user.password_digest)
-                original_user.save(validate: false)
-                current_user.destroy
-                session[:user_id] = original_user.id
+        logged_in_user = User.find_by(id: session[:user_id])
+        already_existing_user = User.find_by(spotify_uid: auth_hash[:uid])
+        signing_in = logged_in_user.spotify_uid.blank? if logged_in_user
+        user_already_exists = !!already_existing_user
 
-                binding.pry
-            else
-                binding.pry
-                current_user = User.find_by(id: session[:user_id])
-                current_user.spotify_username = auth_hash[:info][:display_name]
-                current_user.spotify_uid = auth_hash[:uid]
-                current_user.spotify_email = auth_hash[:info][:email]
-                current_user.save(validate: false)
-
-                binding.pry
-            end
-        else
+        if signing_in && user_already_exists
             binding.pry
-            
-            current_user = User.find_or_create_by(spotify_uid: auth_hash[:uid])
+            already_existing_user.update(username: logged_in_user.username, password_digest: logged_in_user.password_digest)
+            already_existing_user.save(validate: false)
 
-            # Create or update user data from Spotify
-            # If feature is added to edit user info, you can change to a block
+            session[:user_id] = already_existing_user.id
+            logged_in_user.destroy
+        else
+            current_user = User.find_or_create_by(spotify_uid: auth_hash[:uid])
             current_user.spotify_username = auth_hash[:info][:display_name]
             current_user.spotify_email = auth_hash[:info][:email]
             current_user.save(validate: false)
-            
-            session[:user_id] = current_user.id
 
-            binding.pry
+            session[:user_id] = current_user.id
         end
 
-        binding.pry
+
+        # if current_user # is logged in
+
+        #     binding.pry
+        #     original_user = User.find_by(spotify_uid: auth_hash[:uid])
+        #     if current_user.spotify_uid.blank? && original_user
+        #         original_user.update(username: current_user.username, password_digest: current_user.password_digest)
+        #         original_user.save(validate: false)
+        #         current_user.destroy
+        #         session[:user_id] = original_user.id
+
+        #         binding.pry
+        #     else
+        #         binding.pry
+
+        #         #current_user = User.find_by(id: session[:user_id])
+        #         current_user.spotify_username = auth_hash[:info][:display_name]
+        #         current_user.spotify_uid = auth_hash[:uid]
+        #         current_user.spotify_email = auth_hash[:info][:email]
+        #         current_user.save(validate: false)
+
+        #         binding.pry
+        #     end
+        # else
+        #     binding.pry
+            
+        #     current_user = User.find_or_create_by(spotify_uid: auth_hash[:uid])
+
+        #     # Create or update user data from Spotify
+        #     # If feature is added to edit user info, you can change to a block
+        #     current_user.spotify_username = auth_hash[:info][:display_name]
+        #     current_user.spotify_email = auth_hash[:info][:email]
+        #     current_user.save(validate: false)
+            
+        #     session[:user_id] = current_user.id
+
+        #     binding.pry
+        # end
+
+        # binding.pry
         redirect_to user_reports_path(session[:user_id])
     end
 
@@ -58,5 +83,13 @@ class AuthenticationController < ApplicationController
         session.delete(:user_id)
         redirect_to root_path
     end
+
+    # def signing_in?
+    #     logged_in_user.spotify_uid.blank? if logged_in_user
+    # end
+
+    # def user_already_exists?
+    #     !!already_existing_user
+    # end
 
 end
